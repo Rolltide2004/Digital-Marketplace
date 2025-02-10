8e5d3bbed6cbabb4ef958e7ab04853a9f59add98
Ryan Sanders <sandersryan2004@gmail.com>
2025-02-10 17:37:34 -0500

    Maui Added

---
+++ Maui.cop4870/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.cop4870
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Maui.cop4870/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Maui.cop4870
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InventoryClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
