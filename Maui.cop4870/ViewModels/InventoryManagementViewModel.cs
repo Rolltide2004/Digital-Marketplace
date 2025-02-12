213ffaa238bcce7826226430a1dda502f4a0eac8
Ryan Sanders <sandersryan2004@gmail.com>
2025-02-11 21:41:53 -0500

    Delete Frontend Linked

--- Maui.cop4870/ViewModels/InventoryManagementViewModel.cs
+++ Maui.cop4870/ViewModels/InventoryManagementViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using COP4870.Models;
@@ -9,19 +12,31 @@ using COP4870.Services;
 
 namespace Maui.cop4870.ViewModels
 {
-    public class InventoryManagementViewModel
+    public class InventoryManagementViewModel : INotifyPropertyChanged
     {
         public Product? SelectedProduct { get; set; }
         private InventoryServiceProxy _svc = InventoryServiceProxy.Current;
-        public List<Product?> Products
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName="")
+        {
+            if (propertyName is null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ObservableCollection<Product?> Products
         {
             get {
-                return _svc.Products;
+                return new ObservableCollection<Product?>(_svc.Products);
             }
         }
         public Product? Delete()
         {
-            return _svc.Delete(SelectedProduct?.id ?? 0);
+            var item = _svc.Delete(SelectedProduct?.id ?? 0);
+            NotifyPropertyChanged("Products");
+            return item;
         }
     }
 }
