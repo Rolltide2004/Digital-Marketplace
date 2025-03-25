cf411b984c04a539deb6f3aa908521754a891ea1
Ryan Sanders <sandersryan2004@gmail.com>
2025-03-24 20:03:56 -0400

    finished 3/3

--- COP4870/Services/ShoppingCartService.cs
+++ COP4870/Services/ShoppingCartService.cs
@@ -10,7 +10,7 @@ namespace COP4870.Services
 {
     public class ShoppingCartService
     {
-        private InventoryServiceProxy _prodsvc;
+        private InventoryServiceProxy _prodSvc = InventoryServiceProxy.Current;
         private List<Item> items;
         public List<Item> CartItems { 
             get { 
@@ -28,6 +28,29 @@ namespace COP4870.Services
         private static ShoppingCartService? instance;
         private ShoppingCartService() { 
             items = new List<Item>(); 
+        }
+        public Item? AddOrUpdate(Item item) 
+        {
+            var existingInvItem = _prodSvc.GetById(item.Id);
+            if(existingInvItem==null || existingInvItem.Quantity == 0)
+            {
+                return null;
+            }
+            if (existingInvItem != null)
+            {
+                existingInvItem.Quantity--;
+            }
+            var existingItem = CartItems.FirstOrDefault(i => i.Id == item.Id);
+            if (existingItem == null)
+            {
+                var newItem = new Item(item);
+                newItem.Quantity = 1;
+                CartItems.Add(newItem);
+            } else
+            {
+                existingItem.Quantity++;
+            }
+            return existingInvItem;
         }
     }
 }
--- Maui.cop4870/ViewModels/ShoppingManagementViewModel.cs
+++ Maui.cop4870/ViewModels/ShoppingManagementViewModel.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using COP4870.Models;
 using COP4870.Services;
 
 namespace Maui.cop4870.ViewModels
 {
-    public class ShoppingManagementViewModel
+    public class ShoppingManagementViewModel : INotifyPropertyChanged
     {
         private InventoryServiceProxy _invSvc = InventoryServiceProxy.Current;
+        private ShoppingCartService _cartSvc = ShoppingCartService.Current;
+        public Item? SelectedItem { get; set; }
         public ObservableCollection<Item?> Inventory
         {
             get {
                 return new ObservableCollection<Item?>(_invSvc.Products);
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (propertyName is null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void PurchaseItem() {
+            if (SelectedItem != null)
+            {
+                var updatedItem = _cartSvc.AddOrUpdate(SelectedItem);
+                if(updatedItem != null && updatedItem.Quantity > 0) 
+                {
+                    NotifyPropertyChanged(nameof(Inventory));
+                }
             }
         }
     }
