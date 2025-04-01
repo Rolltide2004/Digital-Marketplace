ed3ae657170fd33cd2794c1d5ee5b7fe64a648e8
Ryan Sanders <sandersryan2004@gmail.com>
2025-03-31 20:44:48 -0400

    finished 3/17

--- COP4870/DTO/ProductDTO.cs
+++ COP4870/DTO/ProductDTO.cs
@@ -3,38 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using COP4870.DTO;
+using COP4870.Models;
 
-namespace COP4870.Models
+namespace COP4870.DTO
 {
-    public class Product
+    public class ProductDTO
     {
         public string? Name { get; set; }
         public int Id { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
 
-        public Product(){
+        public ProductDTO()
+        {
             Name = string.Empty;
         }
-        public Product(string? i, int q, double p)
+        public ProductDTO(Product p) {
+            Name = p.Name;
+            Id = p.Id;
+        }
+        public ProductDTO(string? i, int q, double p)
         {
             Name = i;
             Quantity = q;
             Price = p;
         }
-        public string? Display {
+        public string? Display
+        {
             get { return $" {Id}. {Name}"; }//\t{Quantity}\t\t$ {Price}"; }
         }
         public override string ToString()
         {
             return Display ?? string.Empty;
         }
-        public Product(Product p) {
-            Name = p.Name;
-            Id = p.Id;
-        }
-        public Product(ProductDTO p)
+        public ProductDTO(ProductDTO p)
         {
             Name = p.Name;
             Id = p.Id;
--- COP4870/Models/Item.cs
+++ COP4870/Models/Item.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using COP4870.DTO;
 
 namespace COP4870.Models
 {
     public class Item
     {
         public int Id { get; set; }
-        public Product Product { get; set; }
+        public ProductDTO Product { get; set; }
         public int? Quantity { get; set; }
+
+        public ICommand? AddCommand { get; set; }
+
         public override string ToString()
         {
             return Display ?? string.Empty;
@@ -23,13 +28,19 @@ namespace COP4870.Models
             }   
         }
         public Item() {
-            Product = new Product();
+            Product = new ProductDTO();
             Quantity = 0;
+
+            AddCommand = null;
+        }
+        private void DoAdd() { 
         }
         public Item(Item i) {
-            Product = new Product(i.Product);
+            Product = new ProductDTO(i.Product);
             Quantity = i.Quantity;
             Id = i.Id;
+
+            //AddCommand = new Command(DoAdd);
         }
     }
 }
