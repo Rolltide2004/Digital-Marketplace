4b223a922aa143955ef32099587e35fd4e3a1340
Ryan Sanders <sandersryan2004@gmail.com>
2025-04-07 16:59:57 -0400

    added api

---
+++ Api.COP4870/Api.COP4870/Controllers/InventoryController.cs
@@ -0,0 +1,28 @@
+using COP4870.DTO;
+using COP4870.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.COP4870.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class InventoryController : ControllerBase
+{
+    private readonly ILogger<InventoryController> _logger;
+
+    public InventoryController(ILogger<InventoryController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Item?> Get()
+    {
+        return new List<Item?>
+        {
+                new Item{ Product = new ProductDTO{Id=1, Name="Product 1", Quantity=10, Price=1.99 }, Id=1, Quantity=1},
+                new Item{ Product = new ProductDTO{Id=2, Name="Product 2", Quantity=10, Price=1.99 }, Id=2, Quantity=2},
+                new Item{ Product = new ProductDTO{Id=3, Name="Product 3", Quantity=10, Price=1.99 }, Id=3, Quantity=3},
+        };
+    }
+}
